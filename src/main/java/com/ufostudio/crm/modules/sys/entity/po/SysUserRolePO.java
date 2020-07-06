1b8333d7dd77680c5088cdc3d486daa5c50181eb
masonleung2016 <masonleung@163.com>
2020-07-06 11:10:52 +0800

    Create SysUserRolePO.java

---
+++ src/main/java/com/ufostudio/crm/modules/sys/entity/po/SysUserRolePO.java
@@ -0,0 +1,114 @@
+package com.ufostudio.crm.modules.sys.entity.po;
+
+import java.util.Date;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/5 22:19
+ * @Package: com.ufostudio.crm.modules.sys.entity.po
+ */
+
+public class SysUserRolePO {
+    /**
+     *
+     */
+    private Long id;
+
+    /**
+     * 用户ID
+     */
+    private Long userId;
+
+    /**
+     * 角色ID
+     */
+    private Long roleId;
+
+    /**
+     *
+     */
+    private Date gmtCreate;
+
+    /**
+     *
+     */
+    private Date gmtUpdate;
+
+    /**
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * 用户ID
+     *
+     * @return user_id 用户ID
+     */
+    public Long getUserId() {
+        return userId;
+    }
+
+    /**
+     * 用户ID
+     *
+     * @param userId 用户ID
+     */
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * 角色ID
+     *
+     * @return role_id 角色ID
+     */
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    /**
+     * 角色ID
+     *
+     * @param roleId 角色ID
+     */
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    /**
+     * @return gmt_create
+     */
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    /**
+     * @param gmtCreate
+     */
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    /**
+     * @return gmt_update
+     */
+    public Date getGmtUpdate() {
+        return gmtUpdate;
+    }
+
+    /**
+     * @param gmtUpdate
+     */
+    public void setGmtUpdate(Date gmtUpdate) {
+        this.gmtUpdate = gmtUpdate;
+    }
+}
