b73151c22d2158eb519f1825de39aaf861cf9a77
masonleung2016 <masonleung@163.com>
2020-07-06 10:55:42 +0800

    Create QcloudGroup.java

---
+++ src/main/java/com/ufostudio/crm/common/validator/group/QcloudGroup.java
@@ -0,0 +1,11 @@
+package com.ufostudio.crm.common.validator.group;
+
+/**
+ * 腾讯云
+ *
+ * @Author: LCF
+ * @Date: 2020/7/5 18:21
+ * @Package: com.ufostudio.crm.common.validator.group
+ */
+public interface QcloudGroup {
+}
