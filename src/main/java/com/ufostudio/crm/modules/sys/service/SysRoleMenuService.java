fae1c40a922f885346dbb58a654cd92c8d7e4a11
masonleung2016 <masonleung@163.com>
2020-07-06 11:15:51 +0800

    Create SysRoleMenuService.java

---
+++ src/main/java/com/ufostudio/crm/modules/sys/service/SysRoleMenuService.java
@@ -0,0 +1,27 @@
+package com.ufostudio.crm.modules.sys.service;
+
+import java.util.List;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/6 0:33
+ * @Package: com.ufostudio.crm.modules.sys.service
+ */
+
+public interface SysRoleMenuService {
+    /**
+     * 新增或修改角色和菜单关系
+     *
+     * @param roleId
+     * @param menuIdList
+     */
+    void saveOrUpdate(Long roleId, List<Long> menuIdList);
+
+    /**
+     * 根据角色id查询菜单id列表
+     *
+     * @param roleId
+     * @return
+     */
+    List<Long> queryMenuIdList(long roleId);
+}
