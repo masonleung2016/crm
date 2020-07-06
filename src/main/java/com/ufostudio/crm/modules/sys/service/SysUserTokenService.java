020a9dfdf4662026f9b025a6a34afc9e7f84f8e3
masonleung2016 <masonleung@163.com>
2020-07-06 11:16:49 +0800

    Create SysUserTokenService.java

---
+++ src/main/java/com/ufostudio/crm/modules/sys/service/SysUserTokenService.java
@@ -0,0 +1,36 @@
+package com.ufostudio.crm.modules.sys.service;
+
+import com.ufostudio.crm.modules.sys.entity.po.SysUserTokenPO;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/6 0:36
+ * @Package: com.ufostudio.crm.modules.sys.service
+ */
+
+public interface SysUserTokenService {
+    /**
+     * 生成用户token
+     *
+     * @param userId
+     * @return
+     */
+    SysUserTokenPO createToken(long userId);
+
+    /**
+     * 根据用户id获取用户token
+     *
+     * @param userId
+     * @return
+     */
+    SysUserTokenPO getTokenByUserId(long userId);
+
+    /**
+     * 根据token值获取用户token
+     *
+     * @param token
+     * @return
+     */
+    SysUserTokenPO getTokenByToken(String token);
+
+}
