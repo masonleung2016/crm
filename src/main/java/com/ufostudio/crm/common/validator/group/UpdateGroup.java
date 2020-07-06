7b526f85d375e0e20fdcf639ecd636c3e43e0b22
masonleung2016 <masonleung@163.com>
2020-07-06 10:56:41 +0800

    Create UpdateGroup.java

---
+++ src/main/java/com/ufostudio/crm/common/validator/group/UpdateGroup.java
@@ -0,0 +1,12 @@
+package com.ufostudio.crm.common.validator.group;
+
+/**
+ * 更新数据 Group
+ *
+ * @Author: LCF
+ * @Date: 2020/7/5 18:23
+ * @Package: com.ufostudio.crm.common.validator.group
+ */
+
+public interface UpdateGroup {
+}
