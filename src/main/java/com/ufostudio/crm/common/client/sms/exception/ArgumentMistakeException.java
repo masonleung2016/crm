f2773bf06db3ec9f44d1fd2d8d03a28094c145a2
masonleung2016 <masonleung@163.com>
2020-07-06 10:41:46 +0800

    Create ArgumentMistakeException.java

---
+++ src/main/java/com/ufostudio/crm/common/client/sms/exception/ArgumentMistakeException.java
@@ -0,0 +1,9 @@
+package com.ufostudio.crm.common.client.sms.exception;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/5 17:17
+ * @Package: com.ufostudio.crm.common.client.sms.exception
+ */
+public class ArgumentMistakeException extends Exception {
+}
