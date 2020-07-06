183f5dd6c2b2191a109544fe052ea65055979928
masonleung2016 <masonleung@163.com>
2020-07-06 10:50:21 +0800

    Create XssFilter.java

---
+++ src/main/java/com/ufostudio/crm/common/filter/XssFilter.java
@@ -0,0 +1,33 @@
+package com.ufostudio.crm.common.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * XSS过滤
+ *
+ * @Author: LCF
+ * @Date: 2020/7/5 17:52
+ * @Package: com.ufostudio.crm.common.filter
+ */
+
+public class XssFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        XssHttpServletRequestWrapper xssRequest = new XssHttpServletRequestWrapper(
+                (HttpServletRequest) request);
+        chain.doFilter(xssRequest, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
