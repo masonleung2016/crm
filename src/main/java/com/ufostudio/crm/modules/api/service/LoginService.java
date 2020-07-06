0814b3cf66bfa78980c1b4e6fa5b4d7d8a29d3c8
masonleung2016 <masonleung@163.com>
2020-07-06 10:58:03 +0800

    Create LoginService.java

---
+++ src/main/java/com/ufostudio/crm/modules/api/service/LoginService.java
@@ -0,0 +1,18 @@
+package com.ufostudio.crm.modules.api.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 登录
+ *
+ * @Author: LCF
+ * @Date: 2020/7/5 18:33
+ * @Package: com.ufostudio.crm.modules.api.service
+ */
+
+@Service
+public class LoginService {
+    public String test() {
+        return "test";
+    }
+}
