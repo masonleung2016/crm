138f8ef1a766f2de7d2175acd677f8b371401582
masonleung2016 <masonleung@163.com>
2020-07-06 10:55:28 +0800

    Create Group.java

---
+++ src/main/java/com/ufostudio/crm/common/validator/group/Group.java
@@ -0,0 +1,16 @@
+package com.ufostudio.crm.common.validator.group;
+
+import javax.validation.GroupSequence;
+
+/**
+ * 定义校验顺序，如果AddGroup组失败，则UpdateGroup组不会再校验
+ *
+ * @Author: LCF
+ * @Date: 2020/7/5 18:20
+ * @Package: com.ufostudio.crm.common.validator.group
+ */
+
+@GroupSequence ({AddGroup.class, UpdateGroup.class})
+public interface Group {
+
+}
