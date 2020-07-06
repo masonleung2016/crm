01f66db4294109b924736180ccf70b4fdb27e131
masonleung2016 <masonleung@163.com>
2020-07-06 10:55:16 +0800

    Create AliyunGroup.java

---
+++ src/main/java/com/ufostudio/crm/common/validator/group/AliyunGroup.java
@@ -0,0 +1,12 @@
+package com.ufostudio.crm.common.validator.group;
+
+/**
+ * 阿里云
+ *
+ * @Author: LCF
+ * @Date: 2020/7/5 18:20
+ * @Package: com.ufostudio.crm.common.validator.group
+ */
+
+public interface AliyunGroup {
+}
