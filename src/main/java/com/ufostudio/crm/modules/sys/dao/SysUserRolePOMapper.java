8233d7174b268e0ebba42942f3fc743f60fe8d4c
masonleung2016 <masonleung@163.com>
2020-07-06 11:08:57 +0800

    Create SysUserRolePOMapper.java

---
+++ src/main/java/com/ufostudio/crm/modules/sys/dao/SysUserRolePOMapper.java
@@ -0,0 +1,32 @@
+package com.ufostudio.crm.modules.sys.dao;
+
+import com.ufostudio.crm.modules.sys.entity.po.SysUserRolePO;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/5 22:15
+ * @Package: com.ufostudio.crm.modules.sys.dao
+ */
+
+@Repository
+public interface SysUserRolePOMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SysUserRolePO record);
+
+    int insertSelective(SysUserRolePO record);
+
+    SysUserRolePO selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(SysUserRolePO record);
+
+    int updateByPrimaryKey(SysUserRolePO record);
+
+    int deleteByUserId(long userId);
+
+    int insertBatch(Map<String, Object> map);
+
+}
