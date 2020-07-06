5f63f1d28b5fe16283cedf4ef1b2f31d301c4bc4
masonleung2016 <masonleung@163.com>
2020-07-06 11:09:09 +0800

    Create SysUserTokenPOMapper.java

---
+++ src/main/java/com/ufostudio/crm/modules/sys/dao/SysUserTokenPOMapper.java
@@ -0,0 +1,27 @@
+package com.ufostudio.crm.modules.sys.dao;
+
+import com.ufostudio.crm.modules.sys.entity.po.SysUserTokenPO;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/5 22:16
+ * @Package: com.ufostudio.crm.modules.sys.dao
+ */
+
+@Repository
+public interface SysUserTokenPOMapper {
+    int deleteByPrimaryKey(Long userId);
+
+    int insert(SysUserTokenPO record);
+
+    int insertSelective(SysUserTokenPO record);
+
+    SysUserTokenPO selectByPrimaryKey(Long userId);
+
+    int updateByPrimaryKeySelective(SysUserTokenPO record);
+
+    int updateByPrimaryKey(SysUserTokenPO record);
+
+    SysUserTokenPO getTokenByToken(String token);
+}
