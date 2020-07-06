57c831f8a5d1c0fa3bcdaae0702a965ee6b74286
masonleung2016 <masonleung@163.com>
2020-07-06 11:10:00 +0800

    Create SysRoleDeptPO.java

---
+++ src/main/java/com/ufostudio/crm/modules/sys/entity/po/SysRoleDeptPO.java
@@ -0,0 +1,114 @@
+package com.ufostudio.crm.modules.sys.entity.po;
+
+import java.util.Date;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/5 22:17
+ * @Package: com.ufostudio.crm.modules.sys.entity.po
+ */
+
+public class SysRoleDeptPO {
+    /**
+     *
+     */
+    private Long id;
+
+    /**
+     * 角色ID
+     */
+    private Long roleId;
+
+    /**
+     * 部门ID
+     */
+    private Long deptId;
+
+    /**
+     *
+     */
+    private Date gmtCreate;
+
+    /**
+     *
+     */
+    private Date gmtUpdate;
+
+    /**
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * 角色ID
+     *
+     * @return role_id 角色ID
+     */
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    /**
+     * 角色ID
+     *
+     * @param roleId 角色ID
+     */
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    /**
+     * 部门ID
+     *
+     * @return dept_id 部门ID
+     */
+    public Long getDeptId() {
+        return deptId;
+    }
+
+    /**
+     * 部门ID
+     *
+     * @param deptId 部门ID
+     */
+    public void setDeptId(Long deptId) {
+        this.deptId = deptId;
+    }
+
+    /**
+     * @return gmt_create
+     */
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    /**
+     * @param gmtCreate
+     */
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    /**
+     * @return gmt_update
+     */
+    public Date getGmtUpdate() {
+        return gmtUpdate;
+    }
+
+    /**
+     * @param gmtUpdate
+     */
+    public void setGmtUpdate(Date gmtUpdate) {
+        this.gmtUpdate = gmtUpdate;
+    }
+}
