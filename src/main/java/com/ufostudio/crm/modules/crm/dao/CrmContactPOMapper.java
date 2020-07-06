cfa97c29a8874c112c236045cdf48461b916f465
masonleung2016 <masonleung@163.com>
2020-07-06 10:59:36 +0800

    Create CrmContactPOMapper.java

---
+++ src/main/java/com/ufostudio/crm/modules/crm/dao/CrmContactPOMapper.java
@@ -0,0 +1,46 @@
+package com.ufostudio.crm.modules.crm.dao;
+
+import com.ufostudio.crm.modules.crm.entity.po.CrmContactPO;
+import com.ufostudio.crm.modules.crm.entity.po.CrmContactPOExample;
+import com.github.pagehelper.Page;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/5 18:37
+ * @Package: com.ufostudio.crm.modules.crm.dao
+ */
+
+@Repository
+public interface CrmContactPOMapper {
+    long countByExample(CrmContactPOExample example);
+
+    int deleteByExample(CrmContactPOExample example);
+
+    int deleteByPrimaryKey(Long contactId);
+
+    int insert(CrmContactPO record);
+
+    int insertSelective(CrmContactPO record);
+
+    List<CrmContactPO> selectByExample(CrmContactPOExample example);
+
+    CrmContactPO selectByPrimaryKey(Long contactId);
+
+    int updateByExampleSelective(@Param ("record") CrmContactPO record, @Param ("example") CrmContactPOExample example);
+
+    int updateByExample(@Param ("record") CrmContactPO record, @Param ("example") CrmContactPOExample example);
+
+    int updateByPrimaryKeySelective(CrmContactPO record);
+
+    int updateByPrimaryKey(CrmContactPO record);
+
+    Page<CrmContactPO> listMyChargedContactList(Long userId);
+
+    Page<CrmContactPO> listUserPermContactList(CrmContactPO crmContactPO);
+
+    void deleteBatch(Long[] ids);
+}
