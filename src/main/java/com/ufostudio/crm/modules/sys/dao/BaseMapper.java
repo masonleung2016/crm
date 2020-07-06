e6898750b94c9c1027dd608e3fcc5ee7fb4d8c8e
masonleung2016 <masonleung@163.com>
2020-07-06 11:07:31 +0800

    Create BaseMapper.java

---
+++ src/main/java/com/ufostudio/crm/modules/sys/dao/BaseMapper.java
@@ -0,0 +1,18 @@
+package com.ufostudio.crm.modules.sys.dao;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/5 22:12
+ * @Package: com.ufostudio.crm.modules.sys.dao
+ */
+
+public interface BaseMapper {
+
+    /**
+     * 得到
+     *
+     * @return
+     */
+    int getTotalCount();
+
+}
