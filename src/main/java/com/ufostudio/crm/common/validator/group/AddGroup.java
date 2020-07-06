e97c2a44ac5850563705e0715f80bdb55253ee12
masonleung2016 <masonleung@163.com>
2020-07-06 10:55:03 +0800

    Create AddGroup.java

---
+++ src/main/java/com/ufostudio/crm/common/validator/group/AddGroup.java
@@ -0,0 +1,12 @@
+package com.ufostudio.crm.common.validator.group;
+
+/**
+ * 新增数据 Group
+ *
+ * @Author: LCF
+ * @Date: 2020/7/5 18:18
+ * @Package: com.ufostudio.crm.common.validator.group
+ */
+
+public interface AddGroup {
+}
