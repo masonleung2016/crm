8324ffdd1f5a4040269d27a343d19227d3c81a99
masonleung2016 <masonleung@163.com>
2020-07-06 10:40:37 +0800

    Create QiniuClientFatory.java

---
+++ src/main/java/com/ufostudio/crm/common/client/qiniu/QiniuClientFatory.java
@@ -0,0 +1,22 @@
+package com.ufostudio.crm.common.client.qiniu;
+
+import com.ufostudio.crm.common.client.qiniu.impl.DefaultQiniuClient;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/7/5 17:15
+ * @Package: com.ufostudio.crm.common.client.qiniu
+ */
+
+public class QiniuClientFatory {
+    public static QiniuClient qiniuClient;
+
+    static {
+        qiniuClient = new DefaultQiniuClient();
+    }
+
+    public static QiniuClient getQiniuClient() {
+        return qiniuClient;
+    }
+
+}
