d384dd377bf2b1f6feb9821c0aeb691b2a0bffcf
masonleung2016 <masonleung@163.com>
2020-07-06 10:57:05 +0800

    Create Assert.java

---
+++ src/main/java/com/ufostudio/crm/common/validator/Assert.java
@@ -0,0 +1,27 @@
+package com.ufostudio.crm.common.validator;
+
+import com.ufostudio.crm.common.exception.RRException;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * 数据校验
+ *
+ * @Author: LCF
+ * @Date: 2020/7/5 18:24
+ * @Package: com.ufostudio.crm.common.validator.group
+ */
+
+public abstract class Assert {
+
+    public static void isBlank(String str, String message) {
+        if (StringUtils.isBlank(str)) {
+            throw new RRException(message);
+        }
+    }
+
+    public static void isNull(Object object, String message) {
+        if (object == null) {
+            throw new RRException(message);
+        }
+    }
+}
