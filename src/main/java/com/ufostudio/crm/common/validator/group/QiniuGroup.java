7c712b2eaa6d9b2ca3cde6384ce0467cd3346963
masonleung2016 <masonleung@163.com>
2020-07-06 10:56:26 +0800

    Create QiniuGroup.java

---
+++ src/main/java/com/ufostudio/crm/common/validator/group/QiniuGroup.java
@@ -0,0 +1,12 @@
+package com.ufostudio.crm.common.validator.group;
+
+/**
+ * 七牛
+ *
+ * @Author: LCF
+ * @Date: 2020/7/5 18:22
+ * @Package: com.ufostudio.crm.common.validator.group
+ */
+
+public interface QiniuGroup {
+}
