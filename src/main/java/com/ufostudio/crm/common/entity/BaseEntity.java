16a36e7725c784a66b13dbdfff418493d16ee607
masonleung2016 <masonleung@163.com>
2020-07-09 10:16:36 +0800

    Update BaseEntity.java

--- src/main/java/com/ufostudio/crm/common/entity/BaseEntity.java
+++ src/main/java/com/ufostudio/crm/common/entity/BaseEntity.java
@@ -13,24 +13,29 @@ import java.util.Map;
  * @Package: com.ufostudio.crm.common.entity
  */
 
-
 public abstract class BaseEntity<T> implements Serializable {
 
     /**
      * 删除标记（0：正常；1：删除；2：审核；）
      */
     public static final String DEL_FLAG_NORMAL = "0";
+    
     public static final String DEL_FLAG_DELETE = "1";
+    
     public static final String DEL_FLAG_AUDIT = "2";
+    
     private static final long serialVersionUID = 1L;
+    
     /**
      * 当前用户
      */
     protected SysUserPO currentUser;
+    
     /**
      * 当前实体分页对象
      */
     protected PageInfoDTO page;
+    
     /**
      * 自定义SQL（SQL标识，SQL内容）
      */
@@ -71,5 +76,4 @@ public abstract class BaseEntity<T> implements Serializable {
     public String toString() {
         return ReflectionToStringBuilder.toString(this);
     }
-
 }
