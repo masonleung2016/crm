767c29703353e0d614348d8e8ffdce7104347115
masonleung2016 <masonleung@163.com>
2020-07-09 10:14:54 +0800

    Update MyBatisGen.java

--- src/main/java/com/ufostudio/crm/common/config/MyBatisGen.java
+++ src/main/java/com/ufostudio/crm/common/config/MyBatisGen.java
@@ -18,14 +18,23 @@ import java.util.List;
 public class MyBatisGen {
 
     public static void main(String[] args) throws Exception {
+        
         String fileName = "generatorConfig.xml";
+        
         File configFile = new File(fileName);
+        
         List<String> warnings = new ArrayList<String>();
+        
         boolean overwrite = true;
+        
         ConfigurationParser cp = new ConfigurationParser(warnings);
+        
         Configuration config = cp.parseConfiguration(configFile);
+        
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        
         MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        
         myBatisGenerator.generate(null);
     }
 }
