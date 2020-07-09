f344cffe617ad8cee5e42180871ad1827ae4e10e
masonleung2016 <masonleung@163.com>
2020-07-09 10:05:40 +0800

    Update ArgumentMistakeException.java

--- src/main/java/com/ufostudio/crm/common/client/sms/exception/ArgumentMistakeException.java
+++ src/main/java/com/ufostudio/crm/common/client/sms/exception/ArgumentMistakeException.java
@@ -5,5 +5,7 @@ package com.ufostudio.crm.common.client.sms.exception;
  * @Date: 2020/7/5 17:17
  * @Package: com.ufostudio.crm.common.client.sms.exception
  */
+
 public class ArgumentMistakeException extends Exception {
+  
 }
