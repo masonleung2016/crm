64f933a6535435f848a83b8b9c30df2ecf79293e
masonleung2016 <masonleung@163.com>
2020-07-09 10:17:27 +0800

    Update ErrorCodeEnum.java

--- src/main/java/com/ufostudio/crm/common/entity/ErrorCodeEnum.java
+++ src/main/java/com/ufostudio/crm/common/entity/ErrorCodeEnum.java
@@ -6,9 +6,9 @@ package com.ufostudio.crm.common.entity;
  * @Package: com.ufostudio.crm.common.entity
  */
 
-
 @SuppressWarnings ("ALL")
 public enum ErrorCodeEnum {
+    
     NO_POWER(1000, "权限不足"),
     USER_EXIST(1001, "登录名已经存在"),
     USERNAME_PASSSWORD_ERROR(1002, "用户名或密码错误"),
@@ -17,8 +17,8 @@ public enum ErrorCodeEnum {
     user_not_use(1005, "您当前用户被禁用，请与管理员联系"),
     SYSTEM_ERROR(9999, "系统错误");
 
-
     private int status;
+    
     private String msg;
 
     ErrorCodeEnum(int status, String msg) {
@@ -38,7 +38,6 @@ public enum ErrorCodeEnum {
     public int getStatus() {
         return status;
     }
-
 
     public String getMsg() {
         return msg;
