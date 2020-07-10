0ddb7e1baf0b5387426f0876ef23b6ccfb1f79ca
masonleung2016 <masonleung@163.com>
2020-07-10 10:11:47 +0800

    Update SysRoleService.java

--- src/main/java/com/ufostudio/crm/modules/sys/service/SysRoleService.java
+++ src/main/java/com/ufostudio/crm/modules/sys/service/SysRoleService.java
@@ -22,14 +22,12 @@ public interface SysRoleService {
 
     /**
      * 删除角色
-     *
      * @param roleId
      */
     void deleteRole(long roleId);
 
     /**
      * 修改角色
-     *
      * @param loginUser 当前登录用户
      * @param sysRolePO
      */
@@ -37,7 +35,6 @@ public interface SysRoleService {
 
     /**
      * 获取单个角色信息
-     *
      * @param roleId
      * @return
      */
